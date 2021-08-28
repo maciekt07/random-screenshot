55ce64f35a0cd8db9ae68d79d6a57b52a8d574ec
maciekkoks <85953204+maciekkoks@users.noreply.github.com>
2021-08-28 21:57:04 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// prnt sc
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+const randomCharacter = alphabet[Math.floor(Math.random() * alphabet.length)]
+const randomCharacter2 = alphabet[Math.floor(Math.random() * alphabet.length)]
+function n(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+let val = n(1111, 9999);
+link = "https://prnt.sc/" + randomCharacter2 + randomCharacter + val
