a33f32345143a152fb1521ffd84b2ea767ed45fd
maciekkoks <85953204+maciekkoks@users.noreply.github.com>
2021-08-30 12:48:32 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,6 +5,6 @@ const randomCharacter2 = alphabet[Math.floor(Math.random() * alphabet.length)]
 function n(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-let val = n(1111, 9999);
+let val = n(1000, 9999);
 link = "https://prnt.sc/" + randomCharacter2 + randomCharacter + val
 link1 = "https://github.com/maciekkoks"
