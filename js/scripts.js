ee43bde680dbfcc7ee59fe605bfe0fa599ec5f4d
maciekkoks <85953204+maciekkoks@users.noreply.github.com>
2021-11-02 21:27:42 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
-// prnt sc
-const alphabet = "abcdefghijklmnopqrstuvwxyz"
-const randomCharacter = alphabet[Math.floor(Math.random() * alphabet.length)]
-const randomCharacter2 = alphabet[Math.floor(Math.random() * alphabet.length)]
-function n(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+github = "https://github.com/maciekkoks"
+//prntsc
+function prntsc() {
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"
+    const randomCharacter = alphabet[Math.floor(Math.random() * alphabet.length)]
+    const randomCharacter2 = alphabet[Math.floor(Math.random() * alphabet.length)]
+
+    function n(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    let val = n(1000, 9999);
+    link = "https://prnt.sc/" + randomCharacter2 + randomCharacter + val
+    console.log(link);
 }
-let val = n(1000, 9999);
-link = "https://prnt.sc/" + randomCharacter2 + randomCharacter + val
-link1 = "https://github.com/maciekkoks"
+//imgur
+function imgur() {
+    const chars = '01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghiklmnopqrstuvwxyz';
+    var stringlength = 5; /* could be 6 or 7 */
+    var text = '';
+    for (var i = 0; i < stringlength; i++) {
+        var rnum = Math.floor(Math.random() * chars.length);
+        text += chars.substring(rnum, rnum + 1);
+    }
+    imlink = 'https://i.imgur.com/' + text + '.jpg';
+    console.log(imlink);
+}
